cea2960b68a9db8a9f001abbefb0417752d3663a
SUMIT GHOSH <sumitghsh98@gmail.com>
2022-08-23 01:32:07 +0530

    4thproject

---
+++ sportyshoes (1)/sportyshoes/src/main/java/com/project/sportyshoes/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.project.sportyshoes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.sportyshoes.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+		
+	
+	
+}
---
+++ sportyshoes (1)/sportyshoes/src/main/java/com/project/sportyshoes/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.project.sportyshoes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.sportyshoes.model.Role;
+
+
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+	
+}
